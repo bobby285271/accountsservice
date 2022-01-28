e4a0b113ed86cda74413a1b439ebc1a5dda99087
Ray Strode <rstrode@redhat.com>
2022-01-28 14:51:57 -0500

    meson: Determine project version from date
    Numbers are arbitrary, and we've never successfully made it to
    version 1.0 after like 12 years or something.
    
    Furthermore, accountsservice has a very slow release schedule at present
    and some distros hate building from git.
    
    So, I'd like to start generating tarballs more regularly.
    
    Adopting a version number derived from the date will help facilitate
    that.
    
    This commit introduces a script to figure out the version number based
    on the date and the number of commits since the last release.

---
+++ generate-version.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+VERSION_FROM_DIR_NAME=$(dirname $PWD/.. | sed -n 's/^.*-\([^-]*\)$/\1/p')
+
+if [ -n "$VERSION_FROM_DIR_NAME" ]; then
+    echo "$VERSION_FROM_DIR_NAME"
+    exit 0
+fi
+
+COMMITS_SINCE_LAST_RELEASE=$(git rev-list $(git describe --abbrev=0)..HEAD --count)
+date +%y.%V.${COMMITS_SINCE_LAST_RELEASE}
+
