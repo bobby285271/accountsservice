6ca02e63577d2ac01879de8a46cdd4113c36da4c
Ray Strode <rstrode@redhat.com>
2022-02-15 12:59:13 -0500

    meson: Handle weird git clone directory names better
    When generating the version for the project, the first step is
    to determine if the source is from a release or a clone from git.
    
    The script does this by looking at the directory name and seeing
    if it has a hyphen in it. If so, it assumes it's a tarball.
    
    This is a bad heuristic, since clones from git can be put into
    any directory name the cloner wants, including directory names
    with hyphens!
    
    This commit fixes that edge case by explicitly looking for a clone
    from git first.
    
    https://gitlab.freedesktop.org/accountsservice/accountsservice/-/issues/99

--- generate-version.sh
+++ generate-version.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-VERSION_FROM_DIR_NAME=$(dirname $PWD/.. | sed -n 's/^.*-\([^-]*\)$/\1/p')
 
-if [ -n "$VERSION_FROM_DIR_NAME" ]; then
-    echo "$VERSION_FROM_DIR_NAME"
-    exit 0
+# If it's not from a git checkout, assume it's from a tarball
+if [ "$(git rev-parse c5339c5779a67330afbb9406135a7148922478ae)" != "c5339c5779a67330afbb9406135a7148922478ae" ]; then
+    VERSION_FROM_DIR_NAME=$(dirname $PWD/.. | sed -n 's/^accountsservice-\([^-]*\)$/\1/p')
+
+    if [ -n "$VERSION_FROM_DIR_NAME" ]; then
+        echo "$VERSION_FROM_DIR_NAME"
+        exit 0
+    fi
+
+    echo "Source doesn't appear to come from an accountsservice git clone or tarball. Version unknown."
+    exit 1
 fi
 
+# If it is from a git checkout, derive the version from the date of the last commit, and the number
+# of commits since the last release.
 COMMITS_SINCE_LAST_RELEASE=$(git rev-list $(git describe --abbrev=0)..HEAD --count)
 date +%y.%V.${COMMITS_SINCE_LAST_RELEASE} -d "@$(git log -1 --pretty=format:%ct)"
-
