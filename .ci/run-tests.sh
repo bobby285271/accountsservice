f4bf6ffe4d31cf3eedfa03f9c0b798ed8d42fb87
Ray Strode <rstrode@redhat.com>
2022-07-26 12:50:47 -0400

    ci: Fail run-tests if something fails
    I noticed we got a greenlight in CI even though something failed because
    of a missing locale.
    
    This commit adds a set -e to hopefully fix that issue.
    
    It also adds a -v so that there's moar output.

--- .ci/run-tests.sh
+++ .ci/run-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -x
 
+set -e
+
 SRCDIR=`pwd`
 cd /home/user
 cp -r $SRCDIR ./
@@ -7,5 +9,5 @@ cd accountsservice
 meson setup --localstatedir /var -Db_coverage=true _build
 # FIXME until we can figure out how to depend on mocklibc being built for the tests
 meson compile -C _build
-VALGRIND=1 meson test -C _build --print-errorlogs --no-stdsplit --timeout-multiplier 100
+VALGRIND=1 meson test -C _build -v --print-errorlogs --no-stdsplit --timeout-multiplier 100
 ninja -C _build coverage
