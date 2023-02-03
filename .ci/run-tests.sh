a929c4c12375817954bfaedb8ea13568fcbb8b11
Simon McVittie <smcv@debian.org>
2023-02-03 15:42:19 +0000

    CI: Pass --gcov-ignore-parse-errors to gcovr
    Workaround for https://github.com/gcovr/gcovr/issues/710 and similar
    bugs. Because Meson invokes gcovr internally, it doesn't seem to be
    possible to add options any other way.
    
    Signed-off-by: Simon McVittie <smcv@debian.org>

--- .ci/run-tests.sh
+++ .ci/run-tests.sh
@@ -6,6 +6,11 @@ SRCDIR=`pwd`
 cd /home/user
 cp -r $SRCDIR ./
 cd "${CI_PROJECT_NAME}"
+
+# Workaround for https://github.com/gcovr/gcovr/issues/710: replace gcovr
+# with a wrapper that adds --gcov-ignore-parse-errors
+export PATH="$(pwd)/.ci:$PATH"
+
 meson setup --localstatedir /var -Db_coverage=true _build
 # FIXME until we can figure out how to depend on mocklibc being built for the tests
 meson compile -C _build
